848e0de8eee53eafe8bbc4cadc0aaace14002d1d
wantloli <1205u24@gmail.com>
2025-11-11 19:15:19 +0800

    console

--- routes/console.php
+++ routes/console.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:mark-attendance-status')
-    ->dailyAt('17:05')
-    ->timezone('Asia/Manila');
-
-Schedule::command('notifications:cleanup')->daily();
+/**
+ * Schedule your commands here using `php artisan schedule:run`
+ * We'll call them from a cron job
+ */
+Artisan::command('schedule:run', function () {
+    // This manually runs your commands
+    Artisan::call('app:mark-attendance-status');
+    Artisan::call('notifications:cleanup');
+})->describe('Run all scheduled tasks');
